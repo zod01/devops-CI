452ea95b6b83ab5a223b8418d6da10d45070e4fc
amaan <aman78shaikh@protonmail.com>
2026-04-22 20:55:52 +0000

    Github CI

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello world');
+console.log("I'm learning CI/CD using Githubt actions")
---
+++ test.js
@@ -0,0 +1,4 @@
+console.log('Starting test...')
+setTimeout(() => console.log('Waiting 3 seconds..'), 3000);
+console.log('Test complete');
+
